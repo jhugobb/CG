798d9f0cfdcf14eaf94665f6420b8fb972a53432
jhugobb <hugo.b.brandao@gmail.com>
2018-02-06 12:18:51 +0000

    Add initial repo

---
+++ 1/Code/raytracerscene.cpp
@@ -0,0 +1,1 @@
+#include "mainview.h"
---
+++ 1/Code/utility.cpp
@@ -0,0 +1,13 @@
+#include "mainview.h"
+#include "math.h"
+
+/**
+ * @brief MainView::onMessageLogged
+ *
+ * OpenGL logging function, do not change
+ *
+ * @param Message
+ */
+void MainView::onMessageLogged( QOpenGLDebugMessage Message ) {
+    qDebug() << " → Log:" << Message;
+}
