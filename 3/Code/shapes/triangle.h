454d35249fb9d3ad3ce5081cb5c7827b4247e68e
jhugobb <hugo.b.brandao@gmail.com>
2018-02-23 13:22:26 +0100

    Add First part of assignment 3

---
+++ 3/Code/shapes/triangle.h
@@ -0,0 +1,19 @@
+#ifndef TRIANGLE_H_
+#define TRIANGLE_H_
+
+#include "../object.h"
+
+class Triangle : public Object
+{
+  public:
+    Triangle(Point const &pos1, Point const &pos2, Point const &pos3);
+
+    virtual Hit intersect(Ray const &ray);
+
+    Point const v1;
+    Point const v2;
+    Point const v3;
+    Triple const N;
+};
+
+#endif
