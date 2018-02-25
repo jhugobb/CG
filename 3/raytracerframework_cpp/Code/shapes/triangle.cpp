e56be0054e55d416810b3a04210d3ff99bd706bf
MLobo1997 <miguelobo1997@gmail.com>
2018-02-25 18:29:56 +0100

    triangle working in white only

--- 3/raytracerframework_cpp/Code/shapes/triangle.cpp
+++ 3/raytracerframework_cpp/Code/shapes/triangle.cpp
@@ -5,7 +5,35 @@
 using namespace std;
 
 Hit Triangle::intersect(Ray const &ray) {
-    return Hit::NO_HIT();
+    //MT algorithm
+    double u, v;
+    Vector AB = pointB - pointA;
+    Vector AC = pointC - pointA;
+    Vector pV = ray.D.cross(AC);
+    double det = AB.dot(pV);
+    
+    if (det < 0) {
+        return Hit::NO_HIT();
+    }
+
+    double invDet = 1 / det;
+    Vector tV = ray.O - pointA;
+
+    u = tV.dot(pV) * invDet;
+    if (u < 0 || u > 1) {
+        return Hit::NO_HIT();
+    }
+
+    Vector qV = tV.cross(AB);
+    v = ray.D.dot(qV) * invDet;
+
+    if (v < 0 || u + v > 1) {
+        return Hit::NO_HIT();
+    }
+
+    double t = AC.dot(qV) * invDet;
+
+    return Hit(t, AB.cross(AC));
 }
 
 
