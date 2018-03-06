f617eb6879b2b3858718e30ca63520efb46f0cde
jhugobb <hugo.b.brandao@gmail.com>
2018-03-06 14:24:59 +0100

    Add base for project 5

---
+++ 5/Code/shapes/sphere.cpp
@@ -0,0 +1,60 @@
+#include "sphere.h"
+#include "../solveQ.h"
+
+#include <cmath>
+
+using namespace std;
+
+Hit Sphere::intersect(Ray const &ray)
+{
+    /****************************************************
+    * RT1.1: INTERSECTION CALCULATION
+    *
+    * Given: ray, position, r
+    * Sought: intersects? if true: *t
+    *
+    * Insert calculation of ray/sphere intersection here.
+    *
+    * You have the sphere's center (C) and radius (r) as well as
+    * the ray's origin (ray.O) and direction (ray.D).
+    *
+    * If the ray does not intersect the sphere, return false.
+    * Otherwise, return true and place the distance of the
+    * intersection point from the ray origin in *t (see example).
+    ****************************************************/
+
+    // Calculation of the intersections with the sphere with an analytic method
+    Triple OC = ray.O - position;
+    float t0, t1;
+    float a = ray.D.dot(ray.D);
+    float b = 2 * ray.D.dot(OC);
+    float c = OC.dot(OC) - (r*r);
+    if (!solveQuadratic(a, b, c, t0, t1))
+        return Hit::NO_HIT();
+    if (t0 < 0) {
+        t0 = t1; // if t0 is negative, let's use t1 instead
+        if (t0 < 0)
+            return Hit::NO_HIT(); // both t0 and t1 are negative
+    }
+    // Let's use the closest distance to know wich sphere is closer to the camera
+    double t = t0;
+
+    /****************************************************
+    * RT1.2: NORMAL CALCULATION
+    *
+    * Given: t, C, r
+    * Sought: N
+    *
+    * Insert calculation of the sphere's normal at the intersection point.
+    ****************************************************/
+    Vector P = ray.O + ray.D * t;
+    Vector N = (P - position).normalized() ;
+
+    return Hit(t,N);
+}
+
+Sphere::Sphere(Point const &pos, double radius)
+:
+    position(pos),
+    r(radius)
+{}
