95461676e0e259f41c1720bedca6c7a587d1636e
MLobo1997 <miguelobo1997@gmail.com>
2018-03-13 13:21:31 +0100

    Made our program more generic and clean to start the new assignment, with already some implementation: -Created the first animation. -Camera rotation by espheric coordinate system implemented -README Documentation

--- 6/Code/mainview.h
+++ 6/Code/mainview.h
@@ -14,6 +14,7 @@
 #include <memory>
 #include <QMatrix4x4>
 
+#define EYEDISTANCE 10
 class MainView : public QOpenGLWidget, protected QOpenGLFunctions_3_3_Core {
     Q_OBJECT
 
@@ -22,9 +23,10 @@ class MainView : public QOpenGLWidget, protected QOpenGLFunctions_3_3_Core {
 
 
 public:
-    enum MODELINDEX
+    enum MODELINDEX : int
     {
-        MODEL = 0,
+        CAT = 0,
+        SPHERE,
         COUNT
     };
 
@@ -32,16 +34,24 @@ public:
     GLuint vao[MODELINDEX::COUNT]; // [model]
     GLuint texture[MODELINDEX::COUNT]; // [model]
 
-    QMatrix4x4 cubeMatrix;
-    QMatrix4x4 pyramidMatrix;
-    QMatrix4x4 modelMatrix;
-    qreal modelScale = 1.0;
+    QMatrix4x4 objectMatrix[MODELINDEX::COUNT];
     QMatrix4x4 projMatrix = QMatrix4x4();
-    qreal xRotation = 0;
-    qreal yRotation = 0;
-    qreal zRotation = 0;
-    qreal scale = 1;
-    int modelSize;
+    qreal perspectiveRotation = 0;
+    qreal perspectiveHeight = 90; //also in degrees
+    qreal projectionRatio = 1;
+
+    qreal xRotation[MODELINDEX::COUNT];
+    qreal yRotation[MODELINDEX::COUNT];
+    qreal zRotation[MODELINDEX::COUNT];
+    qreal xTranslation[MODELINDEX::COUNT];
+    qreal yTranslation[MODELINDEX::COUNT];
+    qreal zTranslation[MODELINDEX::COUNT];
+    qreal objectScale[MODELINDEX::COUNT];
+    qreal generalScale = 1;
+    qreal xGeneralRotation = 0;
+    qreal yGeneralRotation = 0;
+    qreal zGeneralRotation = 0;
+    int modelSize[MODELINDEX::COUNT];
 
     enum ShadingMode : GLuint
     {
@@ -63,15 +73,17 @@ public:
     ~MainView();
 
     void rotate(qreal x, qreal y, qreal z);
-    void turnCubeToOriginal();
-    void turnPyramidToOriginal ();
-    void turnModelToOriginal();
 
     // Functions for widget input events
-    void setRotation(int rotateX, int rotateY, int rotateZ);
+    void setRotation(qreal rotateX, qreal rotateY, qreal rotateZ);
     void setScale(int scale);
     void setShadingMode(ShadingMode shading);
     void loadTexture(QString file, GLuint texturePtr);
+    void AddRotation(int index, qreal x, qreal y, qreal z);
+    void updateObjects();
+    void loadModel(MODELINDEX modelNr,  char const *objPath, char const *texturePath);
+    void initializeObjectsAttributes();
+    void updateProjectionMatrix();
 
 protected:
     void initializeGL();
