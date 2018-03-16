aefe0e54911f5935f2bc3ce230fdd0ae4cf52d60
MLobo1997 <miguelobo1997@gmail.com>
2018-03-16 18:21:22 +0100

    Phong shading: -implemented the normal transformation correctly -Achieved smoothstep and mixing

--- 6/WaterAnimation/Code/shaders/fragshader_water.glsl
+++ 6/WaterAnimation/Code/shaders/fragshader_water.glsl
@@ -3,14 +3,29 @@
 // Constants
 
 // Inputs to the fragment shader
-in vec3 vertNormal;
+in vec3 N;
+in vec3 materialColor;
+in vec3 vertCoorOut;
+in vec3 eyePosition;
 
 // Uniforms of the fragment shaders
+uniform vec3 lightColor;
+uniform vec4 material;
+uniform vec3 lightPosition;
 
 // Output of the fragment shader
 out vec4 fColor;
 
 void main()
 {
-    fColor = vec4((vertNormal.x + 1.0) / 2.0, (vertNormal.y + 1.0) / 2.0, (vertNormal.z + 1.0) / 2.0, 1.0); //(coor + 1) / 2 is used in order to transform the values that com in a [-1, 1] domain to the [0, 1], domain.
+    vec3 L = normalize(lightPosition - vertCoorOut);
+    vec3 R = reflect(-L, N);
+    vec3 IA = materialColor * material.x;
+    vec3 ID = (max(0.0, dot(L, N)) * lightColor) * materialColor * material.y;
+
+    vec3 IS = (pow(max(0.0, dot(R, eyePosition - vertCoorOut)), material.w) * lightColor) * material.z;
+
+    fColor = vec4((IA + ID + IS), 1.0);
+
+    //fColor = vec4((N.x + 1.0) / 2.0, (N.y + 1.0) / 2.0, (N.z + 1.0) / 2.0, 1.0); //(coor + 1) / 2 is used in order to transform the values that com in a [-1, 1] domain to the [0, 1], domain.
 }
